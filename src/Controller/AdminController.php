c32ff55fb49fc2b2b124f412e0ffbc9001b5f16c
Andrzej Sadowski <andrzej@dux-promotion.pl>
2019-02-28 17:45:06 +0100

    wstepne stworzenie entities

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -6,39 +6,63 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
-
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-
 
 class AdminController extends AbstractController
 {
-    /**
-     * @Route("/login")
-     */
-    public function login(Request $request, SessionInterface $session)
-    {
-        if($request->request->all())
-        {
-          $post = $request->request->all();
-          if(isset($post['login']))
-          {
-            if($post['login'] == 'admin' && $post['haslo'] == 'admin123')
-            {
-              return $this->redirectToRoute('panel');
-            }
-          }
-        }
 
-        return $this->render('admin/login.html.twig',[
+	/**
+	 * @Route("/login")
+	 */
+	public function login(Request $request, SessionInterface $session)
+	{
+		if ($request->request->all()) {
+			$post = $request->request->all();
+			if (isset($post['login'])) {
+				if ($post['login'] == 'admin' && $post['haslo'] == 'admin123') {
+					return $this->redirectToRoute('panel');
+				}
+			}
+		}
 
-        ]);
-    }
+		return $this->render('admin/login.html.twig', [
+		]);
+	}
 
-        /**
-         * @Route("/admin/panel", 'panel')
-         */
-    public function panel()
-    {
-      return new Response('test');
-    }
+
+	/**
+	 * @Route("/admin/panel", name="panel")
+	 */
+	public function panel()
+	{
+		return $this->render('admin/panel.html.twig', [
+		]);
+	}
+	
+	/**
+	 * @Route("/admin/wpisy", name="wpisy")
+	 */
+	public function wpisy()
+	{
+		return $this->render('admin/panel.html.twig', [
+		]);
+	}
+	
+	/**
+	 * @Route("/admin/kategorie", name="kategorie")
+	 */
+	public function kategorie()
+	{
+		return $this->render('admin/panel.html.twig', [
+		]);
+	}
+	
+	/**
+	 * @Route("/admin/wyloguj", name="wyloguj")
+	 */
+	public function wyloguj()
+	{
+		return $this->render('admin/panel.html.twig', [
+		]);
+	}
 }
---
+++ src/Entity/Category.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\CategoryRepository")
+ */
+class Category
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=170)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $content;
+	
+	
+    /**
+     * @ORM\Column(type="datetime", name="created_at")
+     */
+    private $createdAt;
+	
+    /**
+     * @ORM\Column(type="datetime", name="updated_at")
+     */
+    private $updatedAt;
+	
+	
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getLogin(): string
+    {
+        return $this->login;
+    }
+
+    public function setLogin(string $login): self
+    {
+        $this->login = $login;
+
+        return $this;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = sha1($password);
+
+        return $this;
+    }
+	 /**
+     * Gets triggered only on insert
+
+     * @ORM\PrePersist
+     */
+    public function onPrePersist()
+    {
+        $this->createdAt = new \DateTime("now");
+    }
+
+    /**
+     * Gets triggered every time on update
+
+     * @ORM\PreUpdate
+     */
+    public function onPreUpdate()
+    {
+        $this->updatedAt = new \DateTime("now");
+    }
+
+}
